99354c468dfb0bd87dc2395cb2f7f34d96463bbe
seokjae hong <hsj2334@gmail.com>
2018-02-05 17:02:48 +0900

    many-to-many추가

--- django/many_to_many/admin.py
+++ django/many_to_many/admin.py
@@ -2,7 +2,11 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Topping, Pizza
+from .models import Topping, Pizza,Post,User,PostLike
 
 admin.site.register(Topping)
 admin.site.register(Pizza)
+admin.site.register(User)
+admin.site.register(Post)
+admin.site.register(PostLike)
+
--- django/many_to_many/models.py
+++ django/many_to_many/models.py
@@ -1,3 +1,6 @@
+import datetime
+from datetime import timezone
+
 from django.db import models
 
 # Create your models here.
@@ -15,3 +18,46 @@ class Pizza(models.Model):
 
     def __str__(self):
         return  self.name
+
+
+
+#EXTRA FIELDS ON MANY-TO-MANY RELATIONSHIPS
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    like_users = models.ManyToManyField(
+        'User',
+        through='PostLike',
+        related_name='like_posts',
+    )
+
+    def __str__(self):
+        return self.title
+
+class User(models.Model):
+    name= models.CharField(max_length=50)
+
+    def __str__(self):
+        return  self.name
+
+class PostLike(models.Model):
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+    )
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+    )
+    created_date = models.DateTimeField(
+        auto_now_add=True
+    )
+    def __str__(self):
+        # return f'{self.post.title}글의 좋아요({self.user.name},{self.created_date})'
+        return '"{title}"글의 좋아요({name}, {date})'.format(
+            title=self.post.title,
+            name=self.user.name,
+            date=datetime.strftime(
+                timezone.make_navie((self.created_date),'%Y.%m,%d'),
+
+        )
