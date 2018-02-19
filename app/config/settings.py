ce6b273a99ba74ee9b65ed583c2ff3960f8fff14
seokjae hong <hsj2334@gmail.com>
2018-02-19 12:53:36 +0900

    postgresql로 db변경

--- app/config/settings.py
+++ app/config/settings.py
@@ -89,8 +89,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'fc-django-document',
+        'USER': 'hong',
+        'PASSWORD':'1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
+
+
     }
 }
 
